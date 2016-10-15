628539d75e4a3fff30c9ed10d78ca613a03e8f8c
Mark Apinis <mapinis2019@gmail.com>
2016-10-15 18:19:15 -0400

    Added open questions section on index

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-    res.status(200).render('index.pug');
+    res.status(200).render('index.pug', {questions: questions});
 });
 
 app.get('/create', function(req, res){
